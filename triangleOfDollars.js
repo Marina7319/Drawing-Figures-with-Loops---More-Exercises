4752b64f4e7cbfaaec58b47a23811a353d412bfe
Marina7319 <marina-markova@abv.bg>
2022-06-28 13:53:24 +0300

    Commit triangleOfDollars.js

--- triangleOfDollars.js
+++ triangleOfDollars.js
@@ -2,16 +2,13 @@ function triangleOfDollars(input) {
     let index = 0;
     let n = Number(input[index]);
     index++;
-    let buffOne = "$";
     let buffTwo = "";
     for (let row = 0; row < n; row++) {
         buffTwo = "$";
         for (let col = 1; col <= row; col++) {
             buffTwo += " $";
-            //console.log(buffTwo);
         }
         console.log(buffTwo);
-
     }
 }
 triangleOfDollars(
